d1652d87ece1325c558135ad89974d0c1dfbf348
李启发 <823134308@qq.com>
2020-10-18 22:47:43 +0800

    Initial commit

---
+++ src/main/java/com/qifa/generatedocs/GeneratedocsApplication.java
@@ -0,0 +1,13 @@
+package com.qifa.generatedocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneratedocsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeneratedocsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qifa/generatedocs/GeneratedocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qifa.generatedocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneratedocsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
